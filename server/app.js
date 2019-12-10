8b060a4f57b706be40343b180645a5a48922fa44
TinyUniverseDevelopment <humanc0ntact0@gmail.com>
2019-12-10 04:02:00 -0500

    mongo db connected

--- server/app.js
+++ server/app.js
@@ -3,9 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+const multer = require('multer');
+const cors = require('cors');
+const mongoose = require('mongoose');
+var imageRouter = require('./routes/images');
 var usersRouter = require('./routes/users');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(db => {
+    console.log('database is connected');
+  });
 
 var app = express();
 
@@ -15,11 +27,17 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public/images', express.static('images'));
 
-app.use('/', indexRouter);
+app.use('/api/imageupload', imageRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ server/routes/images.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const Image = require('../models/Image')
+const imageRouter = express.Router()
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images')
+    },
+    filename: function (req, file, cd) {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limit: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+})
+
+//store in uploads folder with multer
+imageRouter.route('/uploadmulter').post(upload.single('imageData'), (req, res, next) => {
+    const newImage = new Image({
+        imageName: req.body.imageName,
+        imageData: req.file.path
+    })
+
+    newImage.save().then((result) => {
+        console.log(result)
+        res.status(200).json({
+            success: true,
+            document: result
+        })
+    }).catch(err => next(err))
+})
+
+//store directly in mongodatabase with base64
+imageRouter.route('/uploadbase').post((req, res, next) => {
+    const newImage = new Image({
+        imageName: req.body.imageName,
+        imageData: req.body.imageData
+    })
+
+    newImage.save().then(result => {
+        res.status(200).json({
+            success: true,
+            document: result
+        });
+    }).catch(err => next(err))
+
+})
+
+module.exports = imageRouter
